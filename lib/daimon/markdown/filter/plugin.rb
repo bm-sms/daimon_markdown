695f0070160f524b05426283d10076092aa94f87
Kenji Okimoto <okimoto@clear-code.com>
2016-05-12 13:50:17 +0900

    Implement plugin filter properly

--- lib/daimon/markdown/filter/plugin.rb
+++ lib/daimon/markdown/filter/plugin.rb
@@ -3,10 +3,10 @@ require "daimon/markdown/parser"
 module Daimon
   module Markdown
     module Filter
-      class Plugin < HTML::Pipeline::Filter
+      class Plugin < ::HTML::Pipeline::Filter
         def call
           doc.search(".//text()").each do |node|
-            node.scan(/{{(.+?)}}/) do |str|
+            node.to_s.scan(/{{(.+?)}}/) do |str|
               parser = Daimon::Markdown::Parser.new(str)
               parser.parse
               plugin_class = Daimon::Markdown::Plugin.lookup(parser.name)
@@ -15,6 +15,7 @@ module Daimon
               node.replace(plugin.call(*parser.args))
             end
           end
+          doc
         end
       end
     end
