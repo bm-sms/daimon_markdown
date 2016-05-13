e70642ee429da5fe374f3c995bafba26b8d08f8a
Kenji Okimoto <okimoto@clear-code.com>
2016-05-13 17:34:16 +0900

    Stop processing markdown syntax in plugin arguments

--- lib/daimon/markdown/redcarpet/html_renderer.rb
+++ lib/daimon/markdown/redcarpet/html_renderer.rb
@@ -5,6 +5,32 @@ module Daimon
     module Redcarpet
       class HTMLRenderer < ::Redcarpet::Render::HTML
         include Rouge::Plugins::Redcarpet
+
+        def initialize(extensions = {})
+          super
+          @plugins = []
+        end
+
+        def preprocess(full_document)
+          full_document.scan(/{{.+?}}/) do |m|
+            @plugins << m
+          end
+        end
+
+        def postprocess(full_document)
+          document = ""
+          scanner = StringScanner.new(full_document)
+          loop do
+            if scanner.match?(/{{.+?}}/)
+              document << @plugins.shift
+              scanner.pos += scanner.matched_size
+            else
+              document << scanner.getch
+            end
+            break if scanner.eos?
+          end
+          document
+        end
       end
     end
   end
--- test/plugin/test_figure.rb
+++ test/plugin/test_figure.rb
@@ -18,4 +18,25 @@ class FigureTest < Test::Unit::TestCase
     HTML
     assert_equal(expected_html, doc.to_s.chomp)
   end
+
+  def test_figure_with_markdown_syntax
+    markdown = <<~TEXT
+
+    {{figure("a.png", "alt text", "caption *text*")}}
+    
+    # title
+
+    This is a text
+    TEXT
+    expected_figure = <<~HTML.chomp
+    <figure>
+      <img src="a.png" alt="alt text">
+      <figcaption>caption *text*</figcaption>
+    </figure>
+    HTML
+    processor = Daimon::Markdown::Processor.new
+    result = processor.call(markdown)
+    actual_figure = result[:output].search("figure").first.to_s
+    assert_equal(expected_figure, actual_figure)
+  end
 end
