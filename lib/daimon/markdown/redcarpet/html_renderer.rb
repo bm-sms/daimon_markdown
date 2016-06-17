4c4feec4f3abad4f2207f8d79ca23e4d8b435f91
Kenji Okimoto <okimoto@clear-code.com>
2016-06-18 00:13:08 +0900

    Support math notation (not plugin)
    Example(inline):
    
    This is a math notation: $1+1=2$
    
    Example(multiline):
    
    $$
    1+1=2\\
    2+2=4
    $$

--- lib/daimon/markdown/redcarpet/html_renderer.rb
+++ lib/daimon/markdown/redcarpet/html_renderer.rb
@@ -10,14 +10,19 @@ module Daimon
         def initialize(extensions = {})
           super
           @plugins = []
+          @math_notations = []
         end
 
         def preprocess(full_document)
           scanner = StringScanner.new(full_document)
           loop do
             break if scanner.eos?
-            if scanner.match?(/{{.+?}}/m)
+            case
+            when scanner.match?(/{{.+?}}/m)
               @plugins << scanner.matched
+              scanner.pos += scanner.matched_size
+            when scanner.match?(/\$.+?\$/)
+              @math_notations << scanner.matched
               scanner.pos += scanner.matched_size
             else
               scanner.getch
@@ -27,19 +32,31 @@ module Daimon
         end
 
         def postprocess(full_document)
-          return full_document if @plugins.empty?
+          return full_document if @plugins.empty? && @math_notations.empty?
           document = ""
           scanner = StringScanner.new(full_document)
           loop do
             break if scanner.eos?
-            if scanner.match?(/{{.+?}}/m)
+            case
+            when scanner.match?(/{{.+?}}/m)
               document << @plugins.shift
+              scanner.pos += scanner.matched_size
+            when scanner.match?(/\$.+?\$/)
+              document << @math_notations.shift
               scanner.pos += scanner.matched_size
             else
               document << scanner.getch
             end
           end
           document
+        end
+
+        def block_code(code, lang)
+          if lang == "math"
+            %Q(<div class="math">\n#{code}\n</div>)
+          else
+            super
+          end
         end
       end
     end
